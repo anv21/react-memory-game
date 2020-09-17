36ffc4395bf9ac324d3666088d638c7596f8a623
anv21 <novakanastasya@yandex.ru>
2020-09-17 17:20:14 +0300

    update game heading

--- src/components/MainVeiw/index.jsx
+++ src/components/MainVeiw/index.jsx
@@ -10,7 +10,7 @@ class MainView extends React.Component {
                 <Header/>
                 <article className='main-view-article'>
                     <header>
-                         <h2>The Memory Card Game 1.0</h2>
+                         <h2>The Memory Card Game 1.0.0</h2>
                     </header>
                     <main className='main-view-container'>
                         <ul className="detailed-rules">
